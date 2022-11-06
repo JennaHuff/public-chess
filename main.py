92013f3317b4f2166814b9e3be9842aededc6d70
anton <antonjollant@aol.com>
2022-11-06 19:27:43 +0100

    human-readable colisions handled symbols changed

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ def display_board(board, pieces):      #  pieces = [[0, 1, "W"], [2, 3, "B"]]
     tmp = copy.deepcopy(board)
 
     for piece in pieces:
-        tmp[piece[0]][piece[1]] = piece[2]
+        tmp[piece[1]][piece[0]] = piece[2]
 
     tmp.reverse()
     for lign in tmp:
@@ -29,20 +29,16 @@ def display_board(board, pieces):      #  pieces = [[0, 1, "W"], [2, 3, "B"]]
 
 bd = create_board()
 
-
-wx, bx = 0, 7
-
-# whitePawn = [1, 7, "W"]
-# blackPawn = [6, 0, "B"]
+w_y, b_y = 0, 7
 
 piece_arr = []
 
-for i in range(8):
-    wp = [wx, i, "W"]
+for w_x in range(8):
+    wp = [w_x, w_y, "WP"]
     piece_arr.append(wp)
 
-for i in range(8):
-    bp = [bx, i, "B"]
+for b_x in range(8):
+    bp = [b_x, b_y, "BP"]
     piece_arr.append(bp)
 
 
@@ -52,21 +48,21 @@ while True:
     display_board(bd, piece_arr)
     move = input("Pick a square")
     proper_column = ord(move[0]) - 97 # gives the column in which to check for pawns
+    human_move = int(move[1]) - 1
+    colision_counter = 0
 
 
     for piece in piece_arr:
-        if piece[1] == proper_column and (piece[0] == int(move[1]) - 1 or piece[0] == int(move[1]) + 1):
+        if piece[0] == proper_column and (piece[1] == human_move - 1 or piece[1] == human_move + 1):
             print(f"found the {piece[2]} pawn")
-            if piece[2][0] == "W" and piece[0] < int(move[1]):
-                piece[0] += 1
-            if piece[2][0] == "B" and piece[0] > int(move[1]):
-                piece[0] -= 1
+            if piece[2][0] == "W" and piece[1] < human_move:
+                piece[1] += 1
+                colision_counter += 1
+            if piece[2][0] == "B" and piece[1] > human_move:
+                piece[1] -= 1
+                colision_counter += 1
+        if colision_counter > 1:
+            piece[2] = "#"
+            colision_counter = 0
 
-                 
 
-# e4  -->  e(4-1) and e(4+1) . 
-#          ord(letter) - 97 = 4
-#          e = 4
-# 4   -->   check row 3 and 5
-
- 
