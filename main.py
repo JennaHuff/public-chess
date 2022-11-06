eda41d174c63b456f097cbf0bfc53cff67732f2d
anton <antonjollant@aol.com>
2022-11-06 23:35:24 +0100

    pieces can move freely only on free squares all pieces placed unicode symbols used instead of letters

--- main.py
+++ main.py
@@ -6,11 +6,11 @@ def create_board():
         ligne = []
         for j in range(8):        
             if j % 2 == i % 2:
-                ligne.append(f"{chr(j + 97)}{i + 1}")
-                #ligne.append("X")
+                #ligne.append(f"{chr(j + 97)}{i + 1}")
+                ligne.append("X")
             else:
-                ligne.append(f"{chr(j + 97)}{i + 1}")
-                #ligne.append("O")
+                #ligne.append(f"{chr(j + 97)}{i + 1}")
+                ligne.append("O")
         board.append(ligne)
     return board
     
@@ -29,18 +29,59 @@ def place_pawns(arr):
     w_y, b_y = 1, 6
 
     for w_x in range(8):
-        wp = [w_x, w_y, "WP"]
+        wp = [w_x, w_y, "♙"]
         arr.append(wp)
 
     for b_x in range(8):
-        bp = [b_x, b_y, "BP"]
+        bp = [b_x, b_y, "♟"]
         arr.append(bp)
 
+def place_rooks(arr):
+
+    wr = [0, 0, "♖"]
+    wr2 = [7, 0, "♖"]
+    br = [0, 7, "♜"]
+    br2 = [7, 7, "♜"]
+    arr.append(wr)
+    arr.append(wr2)
+    arr.append(br)
+    arr.append(br2)
+
+def place_knights(arr):
+
+    wr = [1, 0, "♘"]
+    wr2 = [6, 0, "♘"]
+    br = [1, 7, "♞"]
+    br2 = [6, 7, "♞"]
+    arr.append(wr)
+    arr.append(wr2)
+    arr.append(br)
+    arr.append(br2)
+
+def place_bishops(arr):
+
+    wr = [2, 0, "♗"]
+    wr2 = [5, 0, "♗"]
+    br = [2, 7, "♝"]
+    br2 = [5, 7, "♝"]
+    arr.append(wr)
+    arr.append(wr2)
+    arr.append(br)
+    arr.append(br2)
+
+
 def place_queens(arr):
-    w_y = 0
-    for w_x in range(8):
-        wq = [w_x, w_y, "WQ"]
-        arr.append(wq)
+    wq = [3, 0, "♕"]
+    bq = [3, 7, "♛"]
+    arr.append(wq)
+    arr.append(bq)
+
+def place_kings(arr):
+    wq = [4, 0, "♔"]
+    bq = [4, 7, "♚"]
+    arr.append(wq)
+    arr.append(bq)
+
 
 def is_square_free(x, y, arr):
     for piece in arr:
@@ -55,7 +96,10 @@ bd = create_board()
 piece_arr = []
 place_pawns(piece_arr)
 place_queens(piece_arr)
-
+place_rooks(piece_arr)
+place_knights(piece_arr)
+place_bishops(piece_arr)
+place_kings(piece_arr)
 
 while True:
     display_board(bd, piece_arr)
@@ -67,7 +111,7 @@ while True:
     end_y = move[3]
 
     for piece in piece_arr:
-        if piece[0] == ord(start_x) - 97 and piece[1] == int(start_y) - 1 and is_square_free(ord(end_x) - 97, int(end_y) - 1, piece_arr):
+        if piece[0] == ord(start_x) - ord('a') and piece[1] == int(start_y) - 1 and is_square_free(ord(end_x) - ord('a'), int(end_y) - 1, piece_arr):
             piece[0] = ord(end_x) - 97
             piece[1] = int(end_y) - 1
 
